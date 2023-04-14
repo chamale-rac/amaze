a673c0faf5f952b3430b31cc5856e032b408a0be
chamale-rac <samuel.chamale23@gmail.com>
2023-04-14 04:01:36 -0600

    minor changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,15 @@ import * as styles from '@styles/form.module.css'
 function Form() {
   return (
     <div className={styles.container}>
-      <div>Configs</div>
+      <div className={styles.title}>Configs</div>
+      <form>
+        <label>Width:</label>
+        <input type="number" id="w_number" name="w_number" min="4" max="100" />
+        <label>Height:</label>
+        <input type="number" id="h_number" name="h_number" min="4" max="100" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   )
 }
